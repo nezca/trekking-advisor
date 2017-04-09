a5575073fce68dd0b52f37f88e12a50ea0794aa3
nezca <oldyth@gmail.com>
2017-04-09 17:29:52 +0900

    online test

--- index.js
+++ index.js
@@ -16,53 +16,67 @@ app.set('views', './views');
 //app.use(bodyPaser.urlencoded({extended:false}));
 
 //--------- ClearDB(mysql) Setting -------------------
-//var mysql      = require('mysql');
-//var db_config = {
-//  host     : 'us-cdbr-iron-east-04.cleardb.net', 
-//  user     : 'b69910662a1301',
-//  password : '0c76890f',
-//  database : 'heroku_36ce9bdde949664'
-//};
-//var connection;
-//
-//function handleDisconnect() {
-//  connection = mysql.createConnection(db_config);
-//  connection.connect(function(err) {              
-//    if(err) {
-//      console.log('error when connecting to db:', err);
-//      setTimeout(handleDisconnect, 2000);
-//    }                                     
-//  });                                     
-//                                          
-//  connection.on('error', function(err) {
-//    console.log('db error', err);
-//    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-//      handleDisconnect();                         
-//    } else {
-//      throw err;                                  
-//    }
-//  });
-//}
-//
-//
-//handleDisconnect();
+var mysql      = require('mysql');
+var db_config = {
+  host     : 'us-cdbr-iron-east-03.cleardb.net', 
+  user     : 'b52b1fabc5d89f',
+  password : '3fda8c47',
+  database : 'heroku_e54ff6534eba235'
+};
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {              
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }                                     
+  });                                     
+                                          
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();                         
+    } else {
+      throw err;                                  
+    }
+  });
+}
+
+
+handleDisconnect();
 
 //------------ Pages Routing -------------------------
   //------------ 첫(조회) 페이지 -------------------------
 app.get('/', function(req,res){
   res.render('intro');
 });
+  
+  //------------ 결과 페이지 ---------------
+
+app.post('/return', function(req,res){  
+  var sql = 'SELECT * FROM course_table order by altitude_vertical_gap desc limit 3';
+  connection.query(sql, function(err, project_anmg, fields){
+    if(err){
+      console.log(err);
+      res.status(500).send('what the hell!');
+      }
+    res.render('return',{project_anmg:project_anmg});   
+  });
+});  
+
   //------------ 결과 페이지 ---------------
 
 //app.post('/return', function(req,res){
-//  var temporary_address_div = req.body.temporary_address_div;
+//  var country_code = req.body.country_code;
 //  var province_code = req.body.province_code;
-//  var course_type = req.body.course_type;
-//  var duration_rate = req.body.duration_rate;
+//  var course_type_code = req.body.course_type_code;
+//  var duration_rate_code = req.body.duration_rate_code;
 //  var difficulty_index = req.body.difficulty_index; 
 //  var budget = req.body.budget;
-//  var sql = 'SELECT * FROM exporttable WHERE temporary_address_div <= ? and province_code <= ? and course_type <= ? and duration_rate <= ? and difficulty_index <= ? and budget <= ? order by recommend_average desc limit 4';
-//  connection.query(sql,[temporary_address_div, province_code, course_type, duration_rate, difficulty_index, budget], function(err, project_anmg, fields){
+//  var sql = 'SELECT * FROM course_table WHERE country_code <= ? and province_code <= ? and course_type_code <= ? and duration_rate_code <= ? and difficulty_index <= ? and budget <= ? order by altitude_vertical_gap desc limit 3';
+//  connection.query(sql,[country_code, province_code, course_type_code, duration_rate_code, difficulty_index, budget], function(err, project_anmg, fields){
 //    if(err){
 //      console.log(err);
 //      res.status(500).send('what the hell!');
@@ -70,7 +84,6 @@ app.get('/', function(req,res){
 //    res.render('return',{project_anmg:project_anmg});   
 //  });
 //});  
-
 //----- node.js tutorial's app.listen method -----
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
